092b6e4b06b7e34c5b375c0c1d6c0cb1be84dc37
Ashish <ashishkhattry7@gmail.com>
2026-08-14 18:07:47 +0530

    Updated program 4 in chapter 9

--- rust_learning_journey/chapter_09_error_handling/src/bin/04_nested_error_kind.rs
+++ rust_learning_journey/chapter_09_error_handling/src/bin/04_nested_error_kind.rs
@@ -1,20 +1,19 @@
 use std::fs::File;
 use std::io::ErrorKind;
 
-fn main() {// has to be fixed later 
+fn main() {
     let _file = match File::open("secret.txt") {
         Ok(file) => file,
         Err(file_error) => match file_error.kind() {
-         
-            ErrorKind::NotFound => match File::create("secret.txt") {
-                File::create
+   
+            ErrorKind::NotFound => match File::create("secret.txt") { // match कीवर्ड और File::create
                 Ok(fc) => fc,
                 Err(e) => {
                     println!("File creation failed: {:?}", e);
                     panic!("Crashing program!");
                 }
             },
-          
+           
             other_error => {
                 println!("Other error: {:?}", other_error);
                 panic!("Crashing program!");
